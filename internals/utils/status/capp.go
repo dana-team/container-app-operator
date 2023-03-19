62505fcba25a808581b453dfbd3ddc7b6091723a
sahar2339 <xvrahnrubh@gmail.com>
2023-03-19 10:34:06 +0000

    Code comments

--- internals/utils/status/capp.go
+++ internals/utils/status/capp.go
@@ -1,3 +1,5 @@
+// This is a Go package that contains functions for synchronizing the status of a custom resource definition (CRD) called Capp with the status of the Knative service and revisions associated with it.
+// The SyncStatus function is the main function that orchestrates the synchronization process.
 package status_utils
 
 import (
@@ -20,6 +22,7 @@ import (
 
 var openshiftConsoleKey = types.NamespacedName{Namespace: "openshift-console", Name: "console"}
 
+// This function builds the ApplicationLinks status of the Capp  by getting the console route and the cluster segment. It returns a pointer to the ApplicationLinks struct.
 func buildApplicationLinks(ctx context.Context, capp rcsv1alpha1.Capp, log logr.Logger, r client.Client) (*rcsv1alpha1.ApplicationLinks, error) {
 	consoleRoute := routev1.Route{}
 	if err := r.Get(ctx, openshiftConsoleKey, &consoleRoute); err != nil {
@@ -38,6 +41,7 @@ func buildApplicationLinks(ctx context.Context, capp rcsv1alpha1.Capp, log logr.
 	return &applicationLinks, nil
 }
 
+// This function gets the cluster segment from the list of host subnets.
 func getClusterSegment(ctx context.Context, capp rcsv1alpha1.Capp, log logr.Logger, r client.Client) (string, error) {
 	hostsubnets := networkingv1.HostSubnetList{}
 	if err := r.List(ctx, &hostsubnets); err != nil {
@@ -46,6 +50,7 @@ func getClusterSegment(ctx context.Context, capp rcsv1alpha1.Capp, log logr.Logg
 	return hostsubnets.Items[0].Subnet, nil
 }
 
+// This function builds the RevisionInfo status of the Capp CRD by getting the list of revisions associated with the Knative service. It returns a slice of RevisionInfo structs.
 func buildRevisionsStatus(ctx context.Context, capp rcsv1alpha1.Capp, knativeService knativev1.Service, log logr.Logger, r client.Client) ([]rcsv1alpha1.RevisionInfo, error) {
 	knativeRevisions := knativev1.RevisionList{}
 	revisionsInfo := []rcsv1alpha1.RevisionInfo{}
@@ -71,6 +76,8 @@ func buildRevisionsStatus(ctx context.Context, capp rcsv1alpha1.Capp, knativeSer
 	return revisionsInfo, nil
 }
 
+// This is the main function that synchronizes the status of the Capp CRD with the Knative service and revisions associated with it.
+// It gets the Capp CRD, builds the ApplicationLinks and RevisionInfo statuses, and updates the status of the Capp CRD if it has changed.
 func SyncStatus(ctx context.Context, capp rcsv1alpha1.Capp, log logr.Logger, r client.Client) error {
 	cappObject := rcsv1alpha1.Capp{}
 	if err := r.Get(ctx, types.NamespacedName{Namespace: capp.Namespace, Name: capp.Name}, &cappObject); err != nil {
