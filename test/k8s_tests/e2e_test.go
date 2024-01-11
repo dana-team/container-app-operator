a9731fb6fa81c228b887056bbe15744913ef3ec4
liadlauber <liad6500@gmail.com>
2024-01-11 12:09:43 +0200

    Added domainMapping tests
    Signed-off-by: liadlauber <liad6500@gmail.com>

--- test/k8s_tests/e2e_test.go
+++ test/k8s_tests/e2e_test.go
@@ -6,6 +6,9 @@ import (
 	utilst "github.com/dana-team/container-app-operator/test/k8s_tests/utils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	knativev1alphav1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
 	"testing"
 	"time"
 )
@@ -13,6 +16,7 @@ import (
 const (
 	TimeoutCapp          = 60 * time.Second
 	CappCreationInterval = 2 * time.Second
+	CappNamespace        = "capp-e2e-tests"
 )
 
 func TestE2e(t *testing.T) {
@@ -46,6 +50,121 @@ var _ = Describe("Validate Capp adapter", func() {
 		utilst.DeleteCapp(k8sClient, assertionCapp)
 		Eventually(func() bool {
 			return utilst.DoesResourceExist(k8sClient, assertionCapp)
-		}, TimeoutCapp, CappCreationInterval).Should(BeFalse(), "Should not find a resource.")
+		}, TimeoutCapp, CappCreationInterval).ShouldNot(BeTrue(), "Should not find a resource.")
+	})
+})
+
+var _ = Describe("Validate DomainMapping functionality", func() {
+
+	It("Should create DomainMapping when Creating a Capp instance", func() {
+		By("Creating a capp with a route")
+		routeCapp := mock.CreateBaseCapp()
+		routeHostname := utilst.GenerateRouteHostname()
+		routeCapp.Spec.RouteSpec.Hostname = routeHostname
+		utilst.CreateCapp(k8sClient, routeCapp)
+		By("Checking if the domainMapping was created successfully")
+		domainMapping := &knativev1alphav1.DomainMapping{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      routeHostname,
+				Namespace: routeCapp.Namespace,
+			},
+		}
+		Eventually(func() bool {
+			return utilst.DoesResourceExist(k8sClient, domainMapping)
+		}, TimeoutCapp, CappCreationInterval).Should(BeTrue(), "Should find a resource.")
+	})
+
+	It("Should create DomainMapping with secret when Creating an https Capp instance", func() {
+		By("Creating a secret")
+		secretName := utilst.GenerateSecretName()
+		secret := v1.Secret{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      secretName,
+				Namespace: CappNamespace,
+			},
+			Type: "Opaque",
+			Data: map[string][]byte{"extra": []byte("YmFyCg==")},
+		}
+		utilst.CreateSecret(k8sClient, secret)
+		By("Creating an https capp")
+		httpsCapp := mock.CreateBaseCapp()
+		routeHostname := utilst.GenerateRouteHostname()
+		httpsCapp.Spec.RouteSpec.Hostname = routeHostname
+		httpsCapp.Spec.RouteSpec.TlsEnabled = true
+		httpsCapp.Spec.RouteSpec.TlsSecret = secretName
+		utilst.CreateCapp(k8sClient, httpsCapp)
+		By("Checking if the secret reference exists at the domainMapping")
+		Eventually(func() string {
+			domainMapping := utilst.GetDomainMapping(k8sClient, routeHostname, httpsCapp.Namespace)
+			return domainMapping.Spec.TLS.SecretName
+		}, TimeoutCapp, CappCreationInterval).Should(Equal(secretName))
+	})
+
+	It("Should delete DomainMapping when deleting a Capp instance", func() {
+		By("Creating a capp with a route")
+		routeCapp := mock.CreateBaseCapp()
+		routeHostname := utilst.GenerateRouteHostname()
+		routeCapp.Spec.RouteSpec.Hostname = routeHostname
+		createdCapp := utilst.CreateCapp(k8sClient, routeCapp)
+		By("Checking if the domainMapping was created successfully")
+		domainMapping := &knativev1alphav1.DomainMapping{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      routeHostname,
+				Namespace: routeCapp.Namespace,
+			},
+		}
+		Eventually(func() bool {
+			return utilst.DoesResourceExist(k8sClient, domainMapping)
+		}, TimeoutCapp, CappCreationInterval).Should(BeTrue(), "Should find a resource.")
+		CreatedDomainMapping := utilst.GetDomainMapping(k8sClient, routeHostname, routeCapp.Namespace)
+		By("Deleting the capp instance")
+		utilst.DeleteCapp(k8sClient, createdCapp)
+		Eventually(func() bool {
+			return utilst.DoesResourceExist(k8sClient, createdCapp)
+		}, TimeoutCapp, CappCreationInterval).ShouldNot(BeTrue(), "Should not find a resource.")
+		By("Checking if the domainMapping was deleted successfully")
+		Eventually(func() bool {
+			return utilst.DoesResourceExist(k8sClient, CreatedDomainMapping)
+		}, TimeoutCapp, CappCreationInterval).ShouldNot(BeTrue(), "Should not find a resource.")
+	})
+
+	It("Should update DomainMapping when updating route hostname", func() {
+		By("Creating a capp with a route")
+		routeCapp := mock.CreateBaseCapp()
+		routeHostname := utilst.GenerateRouteHostname()
+		routeCapp.Spec.RouteSpec.Hostname = routeHostname
+		createdCapp := utilst.CreateCapp(k8sClient, routeCapp)
+		By("Updating the capp route hostname")
+		toBeUpdatedCapp := utilst.GetCapp(k8sClient, createdCapp.Name, createdCapp.Namespace)
+		updatedRouteHostname := utilst.GenerateRouteHostname()
+		toBeUpdatedCapp.Spec.RouteSpec.Hostname = updatedRouteHostname
+		utilst.UpdateCapp(k8sClient, toBeUpdatedCapp)
+		By("checking if the domainMapping was updated")
+		domainMapping := &knativev1alphav1.DomainMapping{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      updatedRouteHostname,
+				Namespace: toBeUpdatedCapp.Namespace,
+			},
+		}
+		Eventually(func() bool {
+			return utilst.DoesResourceExist(k8sClient, domainMapping)
+		}, TimeoutCapp, CappCreationInterval).Should(BeTrue(), "Should find a resource.")
+	})
+
+	It("Should create DomainMapping without a secret reference when the secret doesn't exist", func() {
+		By("Creating an https capp")
+		secretName := utilst.GenerateSecretName()
+		httpsCapp := mock.CreateBaseCapp()
+		routeHostname := utilst.GenerateRouteHostname()
+		httpsCapp.Spec.RouteSpec.Hostname = routeHostname
+		httpsCapp.Spec.RouteSpec.TlsEnabled = true
+		httpsCapp.Spec.RouteSpec.TlsSecret = secretName
+		utilst.CreateCapp(k8sClient, httpsCapp)
+		By("Checking if the secret reference exists at the domainMapping")
+		Eventually(func() *knativev1alphav1.SecretTLS {
+			domainMapping := utilst.GetDomainMapping(k8sClient, routeHostname, httpsCapp.Namespace)
+			return domainMapping.Spec.TLS
+		}, TimeoutCapp, CappCreationInterval).Should(BeNil())
 	})
 })
--- test/k8s_tests/utils/capp_adapter.go
+++ test/k8s_tests/utils/capp_adapter.go
@@ -4,17 +4,23 @@ import (
 	"context"
 	rcsv1alpha1 "github.com/dana-team/container-app-operator/api/v1alpha1"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	knativev1alphav1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
 	"math/rand"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"time"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
-const RandStrLength = 10
-const TimeoutCapp = 30 * time.Second
-const CappCreationInterval = 2 * time.Second
+const (
+	charset              = "abcdefghijklmnopqrstuvwxyz0123456789"
+	RandStrLength        = 10
+	TimeoutCapp          = 30 * time.Second
+	CappCreationInterval = 2 * time.Second
+	RouteHostname        = "test.dev"
+	RouteTlsSecret       = "https-capp-secret"
+)
 
-var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // generateRandomString returns a random string of the specified length using characters from the charset.
 func generateRandomString(length int) string {
@@ -25,10 +31,21 @@ func generateRandomString(length int) string {
 	return string(b)
 }
 
+// GetResource fetches an existing resource and returns an instance of it.
+func GetResource(k8sClient client.Client, obj client.Object, name, namespace string) {
+	Expect(k8sClient.Get(context.Background(), client.ObjectKey{Name: name, Namespace: namespace}, obj))
+}
+
+// generateName generates a new name by combining the given baseName
+// with a randomly generated string of a specified length.
+func generateName(baseName string) string {
+	randString := generateRandomString(RandStrLength)
+	return baseName + "-" + randString
+}
+
 // CreateCapp creates a new Capp instance with a unique name and returns it.
 func CreateCapp(k8sClient client.Client, capp *rcsv1alpha1.Capp) *rcsv1alpha1.Capp {
-	randString := generateRandomString(RandStrLength)
-	cappName := capp.Name + "-" + randString
+	cappName := generateName(capp.Name)
 	newCapp := capp.DeepCopy()
 	newCapp.Name = cappName
 	Expect(k8sClient.Create(context.Background(), newCapp)).To(Succeed())
@@ -53,6 +70,30 @@ func DeleteCapp(k8sClient client.Client, capp *rcsv1alpha1.Capp) {
 // GetCapp fetch existing and return an instance of Capp.
 func GetCapp(k8sClient client.Client, name string, namespace string) *rcsv1alpha1.Capp {
 	capp := &rcsv1alpha1.Capp{}
-	Expect(k8sClient.Get(context.Background(), client.ObjectKey{Name: name, Namespace: namespace}, capp))
+	GetResource(k8sClient, capp, name, namespace)
 	return capp
+}
+
+// GetDomainMapping fetch existing and return an instance of DomainMapping.
+func GetDomainMapping(k8sClient client.Client, name string, namespace string) *knativev1alphav1.DomainMapping {
+	domainMapping := &knativev1alphav1.DomainMapping{}
+	GetResource(k8sClient, domainMapping, name, namespace)
+	return domainMapping
+}
+
+// CreateSecret creates a new secret.
+func CreateSecret(k8sClient client.Client, secret v1.Secret) {
+	Expect(k8sClient.Create(context.Background(), &secret)).To(Succeed())
+}
+
+// GenerateRouteHostname generates a new route hostname by calling
+// generateName with the predefined RouteHostname as the baseName.
+func GenerateRouteHostname() string {
+	return generateName(RouteHostname)
+}
+
+// GenerateSecretName generates a new secret name by calling
+// generateName with the predefined RouteTlsSecret as the baseName.
+func GenerateSecretName() string {
+	return generateName(RouteTlsSecret)
 }
