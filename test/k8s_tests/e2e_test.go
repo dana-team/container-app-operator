618ff22ef29f2611afe2c89f40c83b64e1b12455
mzeevi <meytar80@gmail.com>
2024-02-08 12:05:26 +0200

    Update e2e_test.go

--- test/k8s_tests/e2e_test.go
+++ test/k8s_tests/e2e_test.go
@@ -14,6 +14,7 @@ import (
 const (
 	TimeoutCapp          = 60 * time.Second
 	CappCreationInterval = 2 * time.Second
+	UnsupportedScaleMetric = "storage"
 	EnabledState         = "enabled"
 	DisabledState        = "disabled"
 )
