7faba371c264e2931350af68928f42166375d816
Shahaf Bahar <sbahar@redhat.com>
2025-12-24 21:27:19 +0200

    feat: scaffold CappBuild API and CRD artifacts
    Add the CappBuild API type and generated CRD manifests.
    Include the CRD in Helm, RBAC helper roles, and a sample
    manifest for reference.
    
    Signed-off-by: Shahaf Bahar <sbahar@redhat.com>

---
+++ api/v1alpha1/cappbuild_types.go
@@ -0,0 +1,157 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type CappBuildSourceType string
+
+const (
+	CappBuildSourceTypeGit CappBuildSourceType = "Git"
+)
+
+type CappBuildRebuildMode string
+
+const (
+	CappBuildRebuildModeInitial  CappBuildRebuildMode = "Initial"
+	CappBuildRebuildModeOnCommit CappBuildRebuildMode = "OnCommit"
+)
+
+type CappBuildSpec struct {
+	// Source refers to the location where the source code is.
+	Source CappBuildSource `json:"source"`
+
+	// +optional
+	// CappRef links this build to a Capp. If set, the controller will update the
+	// Capp with the built image.
+	CappRef *CappBuildCappRef `json:"cappRef,omitempty"`
+
+	// +optional
+	// Rebuild controls rebuild behavior.
+	Rebuild *CappBuildRebuildSpec `json:"rebuild,omitempty"`
+
+	// +optional
+	// Output refers to the location where the built image would be pushed.
+	Output *CappBuildOutputSpec `json:"output,omitempty"`
+}
+
+type CappBuildSource struct {
+	// +kubebuilder:validation:Enum=Git
+	// Type is the type of source code used as input for the build.
+	// Supported values: "Git".
+	Type CappBuildSourceType `json:"type"`
+
+	// Git contains the details for obtaining source code from a git repository.
+	Git CappBuildGitSource `json:"git"`
+
+	// +optional
+	// ContextDir is a path to a subdirectory within the source code that should
+	// be used as the build root directory.
+	ContextDir string `json:"contextDir,omitempty"`
+}
+
+type CappBuildGitSource struct {
+	// +kubebuilder:validation:MinLength=1
+	// URL describes the URL of the Git repository.
+	URL string `json:"url"`
+
+	// +optional
+	// Revision describes the Git revision (e.g., branch, tag, commit SHA, etc.)
+	// to fetch.
+	Revision string `json:"revision,omitempty"`
+
+	// +optional
+	// CloneSecret references a Secret that contains credentials to access the
+	// repository.
+	CloneSecret *corev1.LocalObjectReference `json:"cloneSecret,omitempty"`
+}
+
+type CappBuildCappRef struct {
+	// +kubebuilder:validation:MinLength=1
+	// Name is the name of the Capp resource.
+	Name string `json:"name"`
+
+	// +optional
+	// ContainerName selects which container in the referenced Capp should have its image updated.
+	// If omitted, the first container in the Capp pod template is updated.
+	ContainerName string `json:"containerName,omitempty"`
+}
+
+type CappBuildRebuildSpec struct {
+	// +kubebuilder:validation:Enum=Initial;OnCommit
+	// Mode selects the rebuild strategy.
+	Mode CappBuildRebuildMode `json:"mode"`
+}
+
+type CappBuildOutputSpec struct {
+	// +optional
+	// Image is the reference of the image.
+	Image string `json:"image,omitempty"`
+
+	// +optional
+	// Describes the secret name for pushing a container image.
+	PushSecret *corev1.LocalObjectReference `json:"pushSecret,omitempty"`
+}
+
+type CappBuildStatus struct {
+	// +optional
+	// ObservedGeneration is the .metadata.generation last processed by the
+	// controller.
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// +optional
+	// Conditions represent the latest available observations of the CappBuild's
+	// state.
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// +optional
+	// LatestImage is the last successfully produced image reference.
+	LatestImage string `json:"latestImage,omitempty"`
+
+	// +optional
+	// LastBuildRunRef is a reference to the last BuildRun
+	// created for this CappBuild.
+	LastBuildRunRef string `json:"lastBuildRunRef,omitempty"`
+}
+
+// CappBuild is the Schema for the cappbuilds API. It represents a build-from-
+// source request and its observed state.
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+type CappBuild struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec defines the desired state of the CappBuild.
+	Spec CappBuildSpec `json:"spec,omitempty"`
+	// Status defines the observed state of the CappBuild.
+	Status CappBuildStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type CappBuildList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CappBuild `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CappBuild{}, &CappBuildList{})
+}
