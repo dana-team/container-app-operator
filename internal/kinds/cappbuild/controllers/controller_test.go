42d64f8efb08b175ce558b91eebb3385d9945771
Shahaf Bahar <sbahar@redhat.com>
2026-01-05 13:58:10 +0200

    test(cappbuild): add reconciler unit tests
    Cover missing policy, strategy not found, conflicting build ownership,
    build creation, and drift reconciliation.
    
    Signed-off-by: Shahaf Bahar <sbahar@redhat.com>

---
+++ internal/kinds/cappbuild/controllers/controller_test.go
@@ -0,0 +1,223 @@
+package controllers
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	rcs "github.com/dana-team/container-app-operator/api/v1alpha1"
+	capputils "github.com/dana-team/container-app-operator/internal/kinds/capp/utils"
+	shipwright "github.com/shipwright-io/build/pkg/apis/build/v1beta1"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func testScheme(t *testing.T) *runtime.Scheme {
+	t.Helper()
+
+	s := runtime.NewScheme()
+	require.NoError(t, corev1.AddToScheme(s))
+	require.NoError(t, rcs.AddToScheme(s))
+	require.NoError(t, shipwright.AddToScheme(s))
+	return s
+}
+
+func newReconciler(t *testing.T, objs ...client.Object) (*CappBuildReconciler, client.Client) {
+	t.Helper()
+
+	s := testScheme(t)
+	c := fake.NewClientBuilder().
+		WithScheme(s).
+		WithStatusSubresource(&rcs.CappBuild{}).
+		WithObjects(objs...).
+		Build()
+
+	return &CappBuildReconciler{
+		Client: c,
+		Scheme: s,
+	}, c
+}
+
+const absentStrategy = "absent-strategy"
+
+func newCappConfig() *rcs.CappConfig {
+	return &rcs.CappConfig{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      capputils.CappConfigName,
+			Namespace: capputils.CappNS,
+		},
+		Spec: rcs.CappConfigSpec{
+			CappBuild: &rcs.CappBuildConfig{
+				ClusterBuildStrategy: rcs.CappBuildClusterStrategyConfig{
+					BuildFile: rcs.CappBuildFileStrategyConfig{
+						Present: "present-strategy",
+						Absent:  absentStrategy,
+					},
+				},
+			},
+		},
+	}
+}
+
+func newCappBuild(name, namespace string) *rcs.CappBuild {
+	return &rcs.CappBuild{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:       name,
+			Namespace:  namespace,
+			Generation: 1,
+		},
+		Spec: rcs.CappBuildSpec{
+			BuildFile: rcs.CappBuildFileSpec{Mode: rcs.CappBuildFileModeAbsent},
+			Source: rcs.CappBuildSource{
+				Type: rcs.CappBuildSourceTypeGit,
+				Git:  rcs.CappBuildGitSource{URL: "https://example.invalid/repo.git"},
+			},
+			Output: rcs.CappBuildOutputSpec{Image: "registry.example.com/team/app"},
+		},
+	}
+}
+
+func TestReconcileMissingPolicy(t *testing.T) {
+	ctx := context.Background()
+
+	cb := newCappBuild("cb-"+t.Name(), "ns-"+t.Name())
+
+	r, c := newReconciler(t, cb)
+	res, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Name: cb.Name, Namespace: cb.Namespace}})
+	require.NoError(t, err)
+	require.Greater(t, res.RequeueAfter, time.Duration(0))
+
+	latest := &rcs.CappBuild{}
+	require.NoError(t, c.Get(ctx, client.ObjectKeyFromObject(cb), latest))
+	cond := meta.FindStatusCondition(latest.Status.Conditions, TypeReady)
+	require.NotNil(t, cond, "Ready condition should be set")
+	require.Equal(t, metav1.ConditionFalse, cond.Status)
+	require.Equal(t, ReasonMissingPolicy, cond.Reason)
+}
+
+func TestReconcileStrategyNotFound(t *testing.T) {
+	ctx := context.Background()
+
+	cappConfig := newCappConfig()
+	cb := newCappBuild("cb-"+t.Name(), "ns-"+t.Name())
+
+	r, c := newReconciler(t, cb, cappConfig)
+	res, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Name: cb.Name, Namespace: cb.Namespace}})
+	require.NoError(t, err)
+	require.Greater(t, res.RequeueAfter, time.Duration(0))
+
+	latest := &rcs.CappBuild{}
+	require.NoError(t, c.Get(ctx, client.ObjectKeyFromObject(cb), latest))
+	cond := meta.FindStatusCondition(latest.Status.Conditions, TypeReady)
+	require.NotNil(t, cond, "Ready condition should be set")
+	require.Equal(t, metav1.ConditionFalse, cond.Status)
+	require.Equal(t, ReasonBuildStrategyNotFound, cond.Reason)
+}
+
+func TestReconcileBuildConflict(t *testing.T) {
+	ctx := context.Background()
+
+	cappConfig := newCappConfig()
+	cb := newCappBuild("cb-"+t.Name(), "ns-"+t.Name())
+
+	clusterBuildStrategy := &shipwright.ClusterBuildStrategy{
+		ObjectMeta: metav1.ObjectMeta{Name: absentStrategy},
+	}
+
+	otherOwner := &rcs.CappBuild{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "someone-else",
+			UID:  types.UID("other-uid"),
+		},
+	}
+
+	conflictingBuild := &shipwright.Build{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      buildNameFor(cb),
+			Namespace: cb.Namespace,
+		},
+	}
+	require.NoError(t, controllerutil.SetControllerReference(otherOwner, conflictingBuild, testScheme(t)))
+
+	r, c := newReconciler(t, cb, cappConfig, clusterBuildStrategy, conflictingBuild)
+
+	res, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Name: cb.Name, Namespace: cb.Namespace}})
+	require.NoError(t, err)
+	require.Equal(t, time.Duration(0), res.RequeueAfter)
+
+	latest := &rcs.CappBuild{}
+	require.NoError(t, c.Get(ctx, client.ObjectKeyFromObject(cb), latest))
+	cond := meta.FindStatusCondition(latest.Status.Conditions, TypeReady)
+	require.NotNil(t, cond, "Ready condition should be set")
+	require.Equal(t, metav1.ConditionFalse, cond.Status)
+	require.Equal(t, ReasonBuildConflict, cond.Reason)
+}
+
+func TestReconcileCreatesBuild(t *testing.T) {
+	ctx := context.Background()
+
+	cappConfig := newCappConfig()
+	cb := newCappBuild("cb-"+t.Name(), "ns-"+t.Name())
+
+	clusterBuildStrategy := &shipwright.ClusterBuildStrategy{
+		ObjectMeta: metav1.ObjectMeta{Name: absentStrategy},
+	}
+
+	r, c := newReconciler(t, cb, cappConfig, clusterBuildStrategy)
+	res, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Name: cb.Name, Namespace: cb.Namespace}})
+	require.NoError(t, err)
+	require.Equal(t, time.Duration(0), res.RequeueAfter)
+
+	latest := &rcs.CappBuild{}
+	require.NoError(t, c.Get(ctx, client.ObjectKeyFromObject(cb), latest))
+	require.Equal(t, latest.Generation, latest.Status.ObservedGeneration)
+	require.Equal(t, cb.Namespace+"/"+buildNameFor(cb), latest.Status.BuildRef)
+
+	build := &shipwright.Build{}
+	require.NoError(t, c.Get(ctx, types.NamespacedName{Name: buildNameFor(cb), Namespace: cb.Namespace}, build))
+	require.Equal(t, buildNameFor(cb), build.Name)
+	require.Equal(t, cb.Namespace, build.Namespace)
+	require.True(t, metav1.IsControlledBy(build, latest), "Build should be controller-owned by CappBuild")
+	require.Equal(t, absentStrategy, build.Spec.Strategy.Name)
+}
+
+func TestReconcileUpdatesBuild(t *testing.T) {
+	ctx := context.Background()
+
+	absentStrategy := "absent-strategy"
+	cappConfig := newCappConfig()
+	cb := newCappBuild("cb-"+t.Name(), "ns-"+t.Name())
+
+	clusterBuildStrategy := &shipwright.ClusterBuildStrategy{
+		ObjectMeta: metav1.ObjectMeta{Name: absentStrategy},
+	}
+
+	r, c := newReconciler(t, cb, cappConfig, clusterBuildStrategy)
+
+	res, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Name: cb.Name, Namespace: cb.Namespace}})
+	require.NoError(t, err)
+	require.Equal(t, time.Duration(0), res.RequeueAfter)
+
+	build := &shipwright.Build{}
+	require.NoError(t, c.Get(ctx, types.NamespacedName{Name: buildNameFor(cb), Namespace: cb.Namespace}, build))
+	require.Equal(t, cb.Spec.Source.Git.URL, build.Spec.Source.Git.URL)
+
+	build.Spec.Source.Git.URL = "https://drifted-url.com"
+	require.NoError(t, c.Update(ctx, build))
+
+	res, err = r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Name: cb.Name, Namespace: cb.Namespace}})
+	require.NoError(t, err)
+	require.Equal(t, time.Duration(0), res.RequeueAfter)
+
+	require.NoError(t, c.Get(ctx, types.NamespacedName{Name: buildNameFor(cb), Namespace: cb.Namespace}, build))
+	require.Equal(t, cb.Spec.Source.Git.URL, build.Spec.Source.Git.URL)
+	require.Equal(t, absentStrategy, build.Spec.Strategy.Name)
+}
