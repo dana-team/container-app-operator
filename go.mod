d046947d82b236715acb64b4cfd891db1e3f30f7
Shahaf Bahar <sbahar@redhat.com>
2026-01-08 15:59:06 +0200

    feat(cappbuild): create BuildRun per generation and report build outcome
    Create a Shipwright BuildRun for each CappBuild generation and watch it for
    status updates.
    Set CappBuild BuildSucceeded condition, lastBuildRunRef, and latestImage on
    successful runs.
    Add Shipwright BuildRun RBAC (read/create) and include distref for image ref
    parsing.
    
    Signed-off-by: Shahaf Bahar <sbahar@redhat.com>

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/dana-team/cert-external-issuer v0.1.5
 	github.com/dana-team/nfspvc-operator v0.5.2
 	github.com/dana-team/provider-dns-v2 v1.0.1
+	github.com/distribution/reference v0.6.0
 	github.com/go-logr/logr v1.4.3
 	github.com/go-logr/zapr v1.3.0
 	github.com/kedacore/keda/v2 v2.18.3
--- internal/kinds/cappbuild/controllers/conditions.go
+++ internal/kinds/cappbuild/controllers/conditions.go
@@ -1,11 +1,19 @@
 package controllers
 
 const (
-	TypeReady = "Ready"
+	TypeReady          = "Ready"
+	TypeBuildSucceeded = "BuildSucceeded"
 
 	ReasonBuildReconcileFailed  = "BuildReconcileFailed"
 	ReasonBuildConflict         = "BuildConflict"
 	ReasonBuildStrategyNotFound = "BuildStrategyNotFound"
 	ReasonMissingPolicy         = "MissingPolicy"
 	ReasonReconciled            = "Reconciled"
+
+	ReasonBuildRunReconcileFailed = "BuildRunReconcileFailed"
+	ReasonBuildRunConflict        = "BuildRunConflict"
+	ReasonBuildRunPending         = "BuildRunPending"
+	ReasonBuildRunRunning         = "BuildRunRunning"
+	ReasonBuildRunSucceeded       = "BuildRunSucceeded"
+	ReasonBuildRunFailed          = "BuildRunFailed"
 )
